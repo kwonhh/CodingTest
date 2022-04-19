96f80fed73664c76a747584b39619e99862a16d7
KwonHH-hub <zkzh6hn9@naver.com>
2022-04-19 12:13:54 +0900

    시간초과

--- BOJ/DFS_BFS/BOJ7576.py
+++ BOJ/DFS_BFS/BOJ7576.py
@@ -3,40 +3,24 @@
 
 def stack_solution(n, m, board):    # n : x축, m : y축
     ans = 0
-    #tmt = [m-1, n-1]
-    tmt = []
-    def find_tmt():
+    tmt = []                                #토마토 위치 좌표
+    def find_tmt():                         #토마토 위치를 찾는 함수
         for mm in range(m):
             for nn in range(n):
                 if board[mm][nn] == 1:
                     tmt.append([mm,nn])
-
     find_tmt()
-    stk = []
+
+    stk = []                                #방문할 장소를 담을 리스트
     stk.extend(tmt)
     new_stk = []
-    '''
-    if board[tmt[0] - 1][tmt[1]] == 0:
-        stk.append([tmt[0] - 1, tmt[1]])
-    if board[tmt[0]][tmt[1] - 1] == 0:
-        stk.append([tmt[0], tmt[1] - 1])
-    '''
+
 
     while len(stk) != 0:
         tmt = stk.pop()
         if board[tmt[0]][tmt[1]] == 0:
             board[tmt[0]][tmt[1]] = 1
-        '''
-        if tmt[0]-1 >= 0 and tmt[1] >= 0:
-            if [tmt[0]-1,tmt[1]] not in vis:
-                if [tmt[0]-1,tmt[1]] not in new_stk:
-                    if board[tmt[0]-1][tmt[1]] == 0:
-                        new_stk.append([tmt[0]-1,tmt[1]])
-            if [tmt[0],tmt[1]-1] not in vis:
-                if [tmt[0],tmt[1]-1] not in new_stk:
-                    if board[tmt[0]][tmt[1]-1] == 0:
-                        new_stk.append([tmt[0],tmt[1]-1])
-        '''
+
         for i in range(-1,2):
             for j in range(-1,2):
                 if i*j != 0:
