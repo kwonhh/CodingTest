e1dcf18f15b80d5b08bcc43708c57e26f09663f1
KwonHH-hub <zkzh6hn9@naver.com>
2022-04-26 22:27:58 +0900

    미완료

--- BOJ/DFS_BFS/BOJ1697.py
+++ BOJ/DFS_BFS/BOJ1697.py
@@ -2,17 +2,16 @@ import sys
 from collections import deque
 
 def solution(subin, dest):
-    sec = 0
     q = deque()
     q.append(subin)
     position = [0 for _ in range(dest*5)]
     position[subin] =  1
     while q:
         curr = q.popleft()
-        if position[curr] == dest:
-            print(sec)
+        if curr == dest:
+            print(position[curr])
             break
-        if sec % 2 == 0:
+        if position[curr] % 2 != 0:
             if subin - 1 >= 0:
                 q.append(curr-1)
                 position[curr-1] = position[curr]+1
@@ -20,10 +19,9 @@ def solution(subin, dest):
                 q.append(curr+1)
                 position[curr + 1] = position[curr] + 1
         else:
+            q.append(curr*2)
             position[curr*2] = position[curr]+1
-        print("sec = ", sec, " " ,position)
-        sec += 1
-
+        print("pos = ",position)
 
 if __name__ == '__main__':
     subin, dest = map(int,(sys.stdin.readline().split(' ')))
