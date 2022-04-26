810a788a8805734b140f0347fe050890b0f23550
KwonHH-hub <zkzh6hn9@naver.com>
2022-04-26 15:07:29 +0900

    commit

--- BOJ/DFS_BFS/BOJ2178.py
+++ BOJ/DFS_BFS/BOJ2178.py
@@ -4,40 +4,28 @@ from collections import deque
 x = [0, 0, -1, 1]
 y = [-1, 1, 0, 0]
 
-def find_route(m, n, graph):
-    ans = 1
+def find_route(m, n, graph, visit):
     stk = deque()
     stk.append([0,0])
-    #visit = deque()
-    new_stk = deque()
     while len(stk) != 0:
-        pos = stk.popleft()
-        #visit.append(pos)
-        graph[pos[0]][pos[1]] = 2
+        posy, posx = stk.popleft()
 
-        if pos[0] == m-1 and pos[1] == n-1:
-            #break
-            return ans
-
+        if posy == m-1 and posx == n-1:
+            print(visit[m-1][n-1]+1)
+            break
         for i in range(4):
-            dx = pos[1] + x[i]
-            dy = pos[0] + y[i]
-            if dy > m-1 or dx > n-1 or dy < 0 or dx < 0:
-                continue
-            if graph[dy][dx] == 1:
-                #if [dy, dx] not in visit and [dy, dx] not in stk:
-                new_stk.append([dy, dx])
-        if len(stk) == 0:
-            ans += 1
-            #stk.extend(new_stk)
-            stk += new_stk
-            #new_stk = deque()
-            new_stk = []
-    print(ans)
+            dx = posx + x[i]
+            dy = posy + y[i]
+            if dy < m and dx < n and dy >= 0 and dx >= 0:
+                if visit[dy][dx] == 0 and graph[dy][dx] != 0:
+                    stk.append([dy, dx])
+                    visit[dy][dx] = visit[posy][posx]+1
+
 
 if __name__ == '__main__':
     m, n = map(int, sys.stdin.readline().split())
     # m 이 y축, n 이 x축
     graph = [list(map(int, ' '.join(sys.stdin.readline().split()))) for _ in range(m)]
-    print(find_route(m, n, graph))
+    visit = [[0]*n for _ in range(m)]
+    find_route(m, n, graph, visit)
 
