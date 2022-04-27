093ef99ab12e06030dd725e7b22a3467f94f3824
KwonHH-hub <zkzh6hn9@naver.com>
2022-04-27 15:20:12 +0900

    미완료

--- BOJ/DFS_BFS/BOJ1697.py
+++ BOJ/DFS_BFS/BOJ1697.py
@@ -2,28 +2,32 @@ import sys
 from collections import deque
 
 def solution(subin, dest):
+    visit = [0 for _ in range(100000+1)]
     q = deque()
     q.append(subin)
-    position = [0 for _ in range(dest*5)]
-    position[subin] =  1
     while q:
         curr = q.popleft()
-        if curr == dest:
-            print(position[curr])
-            break
-        if position[curr] % 2 != 0:
-            if subin - 1 >= 0:
-                q.append(curr-1)
-                position[curr-1] = position[curr]+1
-            if subin + 1 <= dest:
-                q.append(curr+1)
-                position[curr + 1] = position[curr] + 1
-        else:
-            q.append(curr*2)
-            position[curr*2] = position[curr]+1
-        print("pos = ",position)
 
+        if curr == dest:
+            print(visit[curr])
+            break
+
+        if 0 <= curr * 2 <= 100000 and visit[curr*2] == 0:
+            visit[curr*2] = visit[curr]+1
+            #visit[curr * 2] = 1
+            q.append(curr*2)
+        if 0 <= curr + 1 <= 100000 and visit[curr+1] == 0:
+            visit[curr+1] = visit[curr]+1
+            #visit[curr + 1] = 1
+            q.append(curr+1)
+        if 0 <= curr - 1<= 100000 and visit[curr-1] == 0:
+            visit[curr-1] = visit[curr]+1
+            #visit[curr-1] = 1
+            q.append(curr-1)
+subin, dest = map(int,(sys.stdin.readline().split(' ')))
+solution(subin, dest)
+'''
 if __name__ == '__main__':
     subin, dest = map(int,(sys.stdin.readline().split(' ')))
-    print(subin, dest)
     solution(subin, dest)
+'''
