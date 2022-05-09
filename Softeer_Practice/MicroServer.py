dd7a94a44e2c5590f1685c91222e8f473e10887a
KwonHH-hub <zkzh6hn9@naver.com>
2022-05-10 01:02:24 +0900

    commit

--- Softeer_Practice/MicroServer.py
+++ Softeer_Practice/MicroServer.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def solution(start, end, service):
     service.sort()
     ans = 0
@@ -9,7 +10,7 @@ def solution(start, end, service):
     # 300MB 이하 서비스의 수 count
     count300 = 0
     for s in service:
-        if s == 300:
+        if s <= 300:
             count300 += 1
 
     # 600MB 초과 서비스의 수 count
@@ -21,34 +22,33 @@ def solution(start, end, service):
 
     st = count300
     ed = end - count600
-    while st <= end and ed >= 0 and st < ed:
-        if st == ed:
-            ans += 1
-            break
-        if service[st] + service[ed] > 900:
-            if count300 != 0:
-                count300 -= 1
-            ans += 1
-            ed -= 1
-        else:
+
+    while st < ed:
+        if service[st] + service[ed] <= 900:
             st += 1
             ed -= 1
             ans += 1
-        if st == ed:
+
+        elif count300 > 0:
+            count300 -= 1
             ans += 1
-            break
+            ed -= 1
+        else:
+            ans += 1
+            ed -= 1
 
+    if st == ed:
+        if count300 > 0:
+            count300 -= 1
+        ans += 1
 
-
-    if count300 != 0:
-        ans += (count300+2) // 3
+    ans += (count300 + 2) // 3
     print(ans)
 
 
-ans = []
 T = int(sys.stdin.readline().rstrip())
 
 for _ in range(T):
     Nserver = int(sys.stdin.readline().rstrip())
     svs = list(map(int, sys.stdin.readline().split()))
-    solution(0, Nserver-1, svs)
+    solution(0, Nserver - 1, svs)
