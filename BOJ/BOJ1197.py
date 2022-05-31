117d9830c0a2a53667cc73c3c8fc1f0b178970f6
KwonHH-hub <zkzh6hn9@naver.com>
2022-05-31 12:10:38 +0900

    commit

--- BOJ/BOJ1197.py
+++ BOJ/BOJ1197.py
@@ -8,22 +8,37 @@ vis = [0] * n_node
 for nn in range(1, n_node+1):
     checker[nn] = nn
 
-def find (node1, node2):
-    if checker[node2] == node2:
-        if node2 > node1:
-            return node1
-        return node2
-    else:
-        root = find(node1, checker[node2])
-        if root > node1:
-            return node1
-        return root
 
-def union (node, root):
-    new_node = checker[node]
-    checker[node] = root
-    if checker[new_node] != root:
-        union(new_node, root)
+'''
+# 실패했던 find 함수
+def find (node):
+    if checker[node] == node:
+        #if node2 > node1:
+        #    return node1
+        return node
+    else:
+        root = find(checker[node])
+        #if root > node1:
+        #    return node1
+        return root
+'''
+def find (node):
+    if checker[node] != node:
+        checker[node] = find(checker[node])
+    return checker[node]
+
+def union (a_node, b_node):
+    a = find(a_node)
+    b = find(b_node)
+    if a == b:
+        return 0
+    if a > b:
+        checker[checker[a_node]] = b
+        checker[a_node] = b
+    else:
+        checker[checker[b_node]] = a
+        checker[b_node] = a
+    return 1
 
 
 for _ in range(n_edge):
@@ -34,21 +49,15 @@ for _ in range(n_edge):
         tree.append((a,b,cost))
 
 s_tree = sorted(tree, key=lambda x:x[2])
-#print(s_tree)
 ans = 0
 leng = 0
 num = 0
 for s in range(n_edge):
-    if checker[s_tree[s][0]] == checker[s_tree[s][1]]:
+    if union(s_tree[s][0], s_tree[s][1]) == 0:
         continue
-
-    root = find(checker[s_tree[s][0]], checker[s_tree[s][1]])
-    union(s_tree[s][0], root)
-    union(s_tree[s][1], root)
-
-    ans += s_tree[s][2]
+    else:
+        ans += s_tree[s][2]
     leng += 1
-    #print("check = ", checker, "(", s_tree[s][0]," <--> ", s_tree[s][1], ")")
     if leng == n_node - 1:
         break
 print(ans)
