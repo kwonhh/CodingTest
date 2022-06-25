bc2e81be1fa0fa2cfd93144fbc61988b372772de
KwonHH-hub <zkzh6hn9@naver.com>
2022-06-26 02:18:33 +0900

    #골드5 #성공 #백트래킹 #브루트포스 #비트마스킹

--- BOJ/BOJ1038.py
+++ BOJ/BOJ1038.py
@@ -10,10 +10,13 @@ else:
     for i in range(1,1024):
         tmp = i
         str_i = ''
+        # 10진수를 2진수의 string으로 변환
         while tmp > 1:
             str_i = str(tmp % 2) + str_i
             tmp //= 2
         str_i = str(tmp) + str_i
+        # 변환된 2진수 문자열과 '9876543210'을 마스킹하여 ans 리스트에 수를 추가
+        # '9876543210'에서 n개를 선택하는 경우, 가능한 경우의 수 중에서 감소하는 수는 1개일 수밖에 없음
         tmp_s = ''
         for s in range(len(str_i),0,-1):
             if str_i[(s*(-1))] == '1':
